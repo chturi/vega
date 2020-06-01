89c0de43695d706bc70f9d21170c9c4261275b86
Christopher Muturi <Muturi@kth.se>
2020-06-01 16:08:02 -0400

    Added VehicleFeatures model

--- Persistence/VegaDbContext.cs
+++ Persistence/VegaDbContext.cs
@@ -6,6 +6,9 @@ namespace vega.Persistence
 {
     public class VegaDbContext : DbContext
     {
+        
+        public DbSet<Make> Makes {get; set;}
+        public DbSet<Feature> Features {get; set;}
 
         public VegaDbContext(DbContextOptions<VegaDbContext> options) 
             : base (options)
@@ -13,12 +16,18 @@ namespace vega.Persistence
             
         }
 
-        public DbSet<Make> Makes {get; set;}
-        public DbSet<Feature> Features {get; set;}
-
         internal object Include(Func<object, object> p)
         {
             throw new NotImplementedException();
+        }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            modelBuilder.Entity<VehicleFeature>().HasKey(vf =>
+            new {vf.VehicleId, vf.FeatureId});
+
+
         }
     }
 }
